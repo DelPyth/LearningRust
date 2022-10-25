cd7475ed127a7e23937d0b6198a8335b666584fa
DelPyth <constantjarod@gmail.com>
2022-10-25 16:50:37 -0400

    Part 5.

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-	let guess = "42".parse().expect("Not a number!");
+	// addition
+	let sum = 5 + 10;
+
+	// subtraction
+	let difference = 95.5 - 4.3;
+
+	// multiplication
+	let product = 4 * 30;
+
+	// division
+	let quotient = 56.7 / 32.2;
+
+	// remainder
+	let remainder = 43 % 5;
 }
