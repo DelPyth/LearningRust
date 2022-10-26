3a58ca65aced87572d47535d83cbaf36ffb8d68d
DelPyth <constantjarod@gmail.com>
2022-10-25 20:57:51 -0400

    Part 5.

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -1,13 +1,10 @@
 fn main() {
-    let number = 6;
-
-	if number % 4 == 0 {
-		println!("Number was divisible by 4");
-	} else if number % 3 == 0 {
-		println!("Number was divisible by 3");
-	} else if number % 2 == 0 {
-		println!("Number was divisible by 2");
+    let condition = true;
+    let number = if condition {
+		5
 	} else {
-		println!("Number was not divisible by 4, 3, or 2");
-	}
+		6
+	};
+
+	println!("The value of number is: {}", number);
 }
